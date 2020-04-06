79295cd6ec26ab198643337a943f46681cf3e8f2
Lucas Andrade <andrattis88@gmail.com>
2020-04-05 22:08:07 -0300

    Initial commit

---
+++ src/components/AddTutorial.js
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import TutorialDataService from "../services/TutorialService";
+
+const AddTutorial = () => {
+
+    const initialTutorialState = {
+        id : null,
+        title : "",
+        description : "",
+        published : false
+    };
+
+    const [tutorial, setTutorial] = useState(initialTutorialState);
+    const [submitted, setSubmitted] = useState(false);
+
+    const handleInputChange = event => {
+        const { name, value } = event.target;
+        setTutorial({...tutorial, [name] : value });
+    }
+
+    const saveTutorial = () => {
+        
+        var data = {
+            title : tutorial.title,
+            description : tutorial.description
+        };
+
+        TutorialDataService.create(data)
+            .then(response => {
+                setTutorial({
+                    id : response.data.id,
+                    title : response.data.title,
+                    description : response.data.description,
+                    published : response.data.published
+                });
+                
+                setSubmitted(true);
+                console.log(response.data);
+            })
+            .catch( e => {
+                console.log(e);
+            });
+
+    }
+
+    const newTutorial = () => {
+        setTutorial(initialTutorialState);
+        setSubmitted(false);
+    }
+
+    return (
+        <div className="submit-form">
+            { submitted ? (
+                <div>
+                    <h4>You submitted successfully!</h4>
+                    <button className="btn btn-success" onClick={newTutorial}>Add</button>
+                </div>
+            ) : (
+                <div>
+
+                    <div className="form-group">
+                        <label htmlFor="title">Title</label>
+                        <input type="text" name="title" id="title" className="form-control" value={tutorial.title} onChange={handleInputChange} required />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="description">Description</label>
+                        <input type="text" name="description" id="description" className="form-control" value={tutorial.description} onChange={handleInputChange} required />
+                    </div>
+
+                    <button onClick={saveTutorial} className="btn btn-success">Submit</button>
+                </div>
+            )}
+        </div>
+    ); 
+        
+    
+};
+
+export default AddTutorial;
